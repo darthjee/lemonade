4fb5aae6064c99860fcf3791849d1b9ee00bdbb1
darthjee <darthjee@gmail.com>
2022-09-28 18:46:04 -0300

    Move file creation to config loader

--- source/app/application.rb
+++ source/app/application.rb
@@ -4,8 +4,6 @@
 #
 # This loads config file into memory and creates the routes
 module Application
-  SAMPLE_CONFIG = 'resource/routes.yml'
-
   class << self
     def config_file_path(path = @config_file_path)
       @config_file_path = path
@@ -18,18 +16,7 @@ module Application
     private
 
     def config
-      Config.load_file(config_file)
-    end
-
-    def config_file
-      return config_file_path if File.exist?(config_file_path)
-
-      folder = config_file_path.gsub(%r{/[^/]*$}, '')
-
-      FileUtils.mkdir_p(folder) if folder && !File.exist?(folder)
-
-      FileUtils.cp(SAMPLE_CONFIG, config_file_path)
-      config_file_path
+      Config.load_file(config_file_path)
     end
   end
 end
--- source/app/config_loader.rb
+++ source/app/config_loader.rb
@@ -1,13 +1,26 @@
 class ConfigLoader
+  SAMPLE_CONFIG = 'resource/routes.yml'
+
   def initialize(path)
     @path = path
   end
 
   def load
-    YAML.load_file(path)
+    YAML.load_file(config_file_path)
   end
 
   private
 
   attr_reader :path
+
+  def config_file_path
+    return path if File.exist?(path)
+
+    folder = path.gsub(%r{/[^/]*$}, '')
+
+    FileUtils.mkdir_p(folder) if folder && !File.exist?(folder)
+
+    FileUtils.cp(SAMPLE_CONFIG, path)
+    path
+  end
 end
