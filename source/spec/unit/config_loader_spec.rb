f3f8b06eb99c22cba85b433d3cfdaf5d4089606a
darthjee <darthjee@gmail.com>
2022-10-04 06:58:39 -0300

    Check if file was created

--- source/spec/unit/config_loader_spec.rb
+++ source/spec/unit/config_loader_spec.rb
@@ -15,6 +15,34 @@ describe ConfigLoader do
     it 'has routes' do
       expect(loader.load['routes']).not_to be_empty
     end
+
+    context 'when the file does not exist' do
+      let(:file_path) do
+        "#{config_folder}/#{config_file}"
+      end
+      let(:config_file)   { "#{SecureRandom.hex(10)}.yml" }
+      let(:config_folder) { "/tmp/#{SecureRandom.hex(10)}" }
+
+      let(:config_path) do
+        "#{config_folder}/#{config_file}"
+      end
+
+      after { FileUtils.rm_rf(config_folder) }
+
+      it 'creates a config file' do
+        expect { loader.load }
+          .to change { Dir[config_path] }
+          .from([])
+          .to([config_path])
+      end
+
+      it 'creates a config folder' do
+        expect { loader.load }
+          .to change { Dir[config_folder] }
+          .from([])
+          .to([config_folder])
+      end
+    end
   end
 end
 
