8d83bb733d52728425a324899a2205cbdb787ed6
darthjee <darthjee@gmail.com>
2022-10-06 14:25:44 -0300

    Add spec over reapply

--- source/spec/unit/route_spec.rb
+++ source/spec/unit/route_spec.rb
@@ -2,12 +2,48 @@
 
 require 'spec_helper'
 
-describe Route do
+describe Route, type: :controller do
   subject(:route) { described_class.new(attributes) }
 
+  let(:app)        { Sinatra::Application }
   let(:path)       { "/route/#{SecureRandom.hex(16)}" }
   let(:content)    { "Content: #{SecureRandom.hex(16)}" }
   let(:attributes) { { path: path, content: content } }
+
+  describe '#apply' do
+    it 'builds the route' do
+      expect { route.apply }
+        .to change { get(path); last_response.status }
+        .from(404)
+        .to(200)
+    end
+
+    it 'builds the route with content' do
+      expect { route.apply }
+        .to change { get(path); last_response.body }
+        .to(content)
+    end
+
+    context 'when there was already another route' do
+      let(:old_content) { "Old Content: #{SecureRandom.hex(16)}" }
+
+      before do
+        described_class.new(path: path, content: old_content).apply
+      end
+
+      it 'rebuilds the same route' do
+        expect { route.apply }
+          .not_to change { get(path); last_response.status }
+      end
+
+      it 'builds the route with new content' do
+        expect { route.apply }
+          .to change { get(path); last_response.body }
+          .from(old_content)
+          .to(content)
+      end
+    end
+  end
 
   describe '#path' do
     it do
