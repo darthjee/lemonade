281f0073138418df898e856c337c47d796609376
darthjee <darthjee@gmail.com>
2022-10-06 15:47:09 -0300

    Refactor application to be resetable

--- source/app/application.rb
+++ source/app/application.rb
@@ -3,20 +3,32 @@
 # Application contains the application definition
 #
 # This loads config file into memory and creates the routes
-module Application
+class Application
   class << self
     def config_file_path(path = @config_file_path)
       @config_file_path = path
     end
 
-    def start
-      config.routes.each(&:apply)
+    def instance
+      @instance ||= new
     end
 
-    private
-
-    def config
-      Config.load_file(config_file_path)
+    def reset
+      @instance = nil
     end
+
+    delegate :start, to: :instance
+  end
+
+  def start
+    config.routes.each(&:apply)
+  end
+
+  private
+
+  delegate :config_file_path, to: :class
+
+  def config
+    @config ||= Config.load_file(config_file_path)
   end
 end
